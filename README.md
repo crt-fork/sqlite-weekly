460e7826bff5d9610d4cedf7f62d5ee463461425
Anton <m@antonz.org>
2022-09-14 00:54:15 +0300

    SQLite Weekly

---
+++ README.md
@@ -0,0 +1,8 @@
+# SQLite Weekly
+
+Every week I publish important news, notable articles, handy extensions,
+tricky questions, useful code samples and other awesome SQLite-related stuff.
+
+Watch this repo to get an email when the next issue is published.
+
+[read the latest issue](https://github.com/nalgeon/sqlite-weekly/releases/latest)
---
+++ app.js
@@ -0,0 +1,32 @@
+const ui = {
+    exec: document.querySelector("#query-exec"),
+    result: document.querySelector("#query-result"),
+};
+
+function onExec() {
+    fetch("latest.json")
+        .then((response) => response.json())
+        .then((issue) => showResult(issue));
+}
+
+function showResult(issue) {
+    ui.result.classList.remove("hidden");
+    ui.result.innerHTML = "";
+    addListItem(ui.result, `<strong>Issue #${issue.number}`);
+    for (let item of issue.items) {
+        let html = `<a href=${item.url}>${item.title}</a>`;
+        if (item.author) {
+            html += ` by ${item.author}`;
+        }
+        addListItem(ui.result, html);
+    }
+    addListItem(ui.result, `[ <a href="${issue.url}">read more</a> ]`);
+}
+
+function addListItem(ul, html) {
+    const li = document.createElement("li");
+    li.innerHTML = html;
+    ul.appendChild(li);
+}
+
+ui.exec.addEventListener("click", onExec);
